0821138e8162524caac69faa1a5a33d25a5a2c7f
Dimuth De Zoysa <dimuthsakya@protonmail.com>
2023-01-19 13:21:42 +0530

    Create constants.dart

---
+++ src/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const kTColorBlue = Colors.lightBlueAccent;
+const kTColorPink = Color(0xAAFF00AA);
+
+const kTColorNum = Color(0xFFDE4D73);
+
+const kTColorBMIVal = Color(0xFFDE3D67);
+
+const kPlusMinusBtnColor = Colors.blue;
